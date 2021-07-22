7b45d57aef06a46f9a892fba2c9a997f56950796
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-22 17:46:03 +0300

    add: new client on server command

---
+++ HECSServer/Server/Commands/NewClientOnServerCommand.cs
@@ -0,0 +1,11 @@
+﻿using HECSFramework.Core;
+using System;
+
+namespace Commands
+{
+    [Documentation("Client", "Эту команду мы отправляем после того как зарегестрировали клиента")]
+    public struct NewClientOnServerCommand : IGlobalCommand
+    {
+        public Guid Client;
+    }
+}
